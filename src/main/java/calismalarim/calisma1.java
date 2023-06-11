afd41234349ee1cc4215241850dbb438d597d350
volkansenel <volkansenel35@gmail.com>
2023-06-11 20:38:31 +0300

    yapilan degisikligi geri aldım

--- src/main/java/calismalarim/calisma1.java
+++ src/main/java/calismalarim/calisma1.java
@@ -2,7 +2,6 @@ package calismalarim;
 
 public class calisma1 {
     public static void main(String[] args) {
-        System.out.println("code degisikligim");
         double sayi = 123.4567;
         double sayi1=12.366;
         int sayi2=12345;
