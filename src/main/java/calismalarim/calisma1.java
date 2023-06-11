6bdad1ae00e3ba1726cea55753a67d938f1a5b9a
volkansenel <volkansenel35@gmail.com>
2023-06-11 20:31:21 +0300

    calisma dosyasina veri güncellemesi yaptım

--- src/main/java/calismalarim/calisma1.java
+++ src/main/java/calismalarim/calisma1.java
@@ -2,8 +2,9 @@ package calismalarim;
 
 public class calisma1 {
     public static void main(String[] args) {
+        System.out.println("code degisikligim");
         double sayi = 123.4567;
-        double sayi1=12.345;
+        double sayi1=12.366;
         int sayi2=12345;
         String str="merhaba volkan";
         System.out.printf("%15.3f%n%15.3f%n",sayi,sayi1);
